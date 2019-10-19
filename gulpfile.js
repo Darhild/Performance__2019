975d5bf2141c0c2f437ece0ec612e39195ac284a
Darhi <isaeva-mariya@yandex.ru>
2019-10-19 23:15:18 +0300

    added css minification

---
+++ gulpfile.js
@@ -0,0 +1,65 @@
+const gulp = require('gulp');
+const plumber = require('gulp-plumber');
+const postcss = require('gulp-postcss');
+const preset = require('postcss-preset-env');
+const concat = require('gulp-concat');
+const csso = require('gulp-csso');
+const rename = require('gulp-rename');
+const svgmin = require('gulp-svgmin');
+const sprite = require('gulp-svg-sprite')
+const server = require('browser-sync').create();
+
+gulp.task('css', () =>  {
+  return gulp.src('./src/css/*.css')
+    .pipe(plumber())
+    .pipe(postcss([preset()]))
+    .pipe(concat('style.css'))
+    .pipe(gulp.dest('./build/css'))
+    .pipe(csso({
+      sourceMap: true,
+    }))
+    .pipe(rename({  suffix: '.min' }))
+    .pipe(gulp.dest('./build/css'))
+    .pipe(server.stream())
+});
+
+gulp.task('js', function(){
+  return gulp.src('./src/js/**/*.js')
+
+    .pipe(gulp.dest('./js'))    
+    .pipe(server.reload({stream: true}))
+});
+
+gulp.task('svg', () => {
+  return gulp.src('./img/svg/*.svg')
+  .pipe(plumber())
+  .pipe(svgmin({
+    js2svg: {
+      pretty: true
+    }
+  }))
+  .pipe(sprite({
+    mode: {
+      inline: true,      
+      symbol: {        
+        sprite: '../sprite.svg'
+      }
+    }
+  }))
+  .pipe(gulp.dest('./img'))
+})
+
+gulp.task("serve", function () {
+  server.init({
+    server: {
+      baseDir: "./build"
+    },
+    notify: false,
+    open: true,
+    cors: true,
+    ui: false
+  });
+
+  gulp.watch("./**/*.css", gulp.series("css"));
+  gulp.watch("./**/*.js", gulp.series("js"));
+});
